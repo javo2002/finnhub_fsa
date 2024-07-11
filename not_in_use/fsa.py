a366e1ddcce3cdf13596ed748ef0ad291aeaceff
javo2002 <javi22.college@gmail.com>
2024-07-11 11:55:17 -0400

    Delete not_in_use directory

--- not_in_use/fsa.py
+++
@@ -1,4 +0,0 @@
-import finnhub
-finnhub_client = finnhub.Client(api_key="cpmethpr01quf620vds0cpmethpr01quf620vdsg")
-
-print(finnhub_client.company_news('AAPL', _from="2024-06-01", to="2024-06-10"))
