69cb7e2a6fd460c6d1d8ed00fd8fc35731006944
javo2002 <javi22.college@gmail.com>
2024-07-27 13:49:55 -0400

    Update fv_api_screener.py

--- fv_api_screener.py
+++ fv_api_screener.py
@@ -1,19 +1,18 @@
 from dotenv import load_dotenv
 import os
 import requests
-import time
-import schedule
-from datetime import datetime
 
 
 def export_screener_data():
     # Define the base screener URL
     base_url = "https://elite.finviz.com/export.ashx"
-    filters = "v=152&p=i1&f=cap_0.01to,geo_usa|china|france|europe|australia|belgium|canada|chinahongkong|germany|hongkong|iceland|japan|newzealand|ireland|netherlands|norway|singapore|southkorea|sweden|taiwan|unitedarabemirates|unitedkingdom|switzerland|spain,sh_curvol_o100,sh_price_u50,sh_relvol_o2,ta_change_u&ft=4&o=sharesfloat&r=81&ar=10"
+    filters = "v=152&p=i1&f=cap_0.01to,geo_usa%7Cchina%7Cfrance%7Ceurope%7Caustralia%7Cbelgium%7Ccanada%7Cchinahongkong%7Cgermany%7Chongkong%7Ciceland%7Cjapan%7Cnewzealand%7Cireland%7Cnetherlands%7Cnorway%7Csingapore%7Csouthkorea%7Csweden%7Ctaiwan%7Cunitedarabemirates%7Cunitedkingdom%7Cswitzerland%7Cspain,sh_curvol_o100,sh_price_u50,sh_relvol_o2,ta_change_u&ft=4&o=sharesfloat&ar=10&c=0,1,2,5,6,25,26,27,28,29,30,84,45,50,51,68,60,61,63,64,67,65,66"
 
 
     # Load environment variables from .env file
     load_dotenv()
+    
+    # print(os.getenv("FINVIZ_API_TOKEN"))
 
     # Access the environment variable
     auth_token = os.getenv("FINVIZ_API_TOKEN")
@@ -29,20 +28,5 @@ def export_screener_data():
 
     print("Exported data to screener.csv")
 
-
-def job():
+if __name__ == "__main__":
     export_screener_data()
-    schedule.every(1).minutes.do(export_screener_data)
-
-
-# Schedule the job to start at 07:00:00
-schedule.every().day.at("7:00:00").do(job)
-
-# Keep the script running and stop at 12:00:00
-while True:
-    current_time = datetime.now().time()
-    if current_time >= datetime.strptime("12:00:00", "%H:%M:%S").time():
-        print(" Screener.csv stopped updating at 12:00:00")
-        break
-    schedule.run_pending()
-    time.sleep(1)
