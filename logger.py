b963f63bff1d636fac8e45573dc37dea6b17516e
javo2002 <javi22.college@gmail.com>
2024-07-27 19:48:14 -0400

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,35 +1,49 @@
-import pandas as pd
 import logging
+import csv
+import datetime
 
-# Set up logging configuration
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler('sentiment_data_df.log'),
-        logging.StreamHandler()
-    ]
-)
+# Custom logging filter to add custom attributes to log records
+class CustomFilter(logging.Filter):
+    def filter(self, record):
+        if not hasattr(record, 'ticker'):
+            record.ticker = ''
+        if not hasattr(record, 'body_sentiment'):
+            record.body_sentiment = ''
+        if not hasattr(record, 'trend_sentiment'):
+            record.trend_sentiment = ''
+        return True
 
-# Create a logger
-logger = logging.getLogger(__name__)
+# Custom formatter to add separators and centered timestamp
+class CustomFormatter(logging.Formatter):
+    def format(self, record):
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        centered_timestamp = f"{timestamp:^80}"
+        separator = '=' * 80
+        log_entry = super().format(record)
+        return f"\n\n{separator}\n{centered_timestamp}\n{separator}\n{log_entry}\n{separator}\n\n"
 
+# Configure the logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-# Function to read CSV and log information
-def log_csv_info():
-    try:
-        # Replace the path with your actual file path
-        file_path = 'news_data_with_sentiments.csv'
-        df = pd.read_csv(file_path)
-        pd.set_option('display.max_rows', None)
-        pd.set_option('display.max_columns', None)
-        # Log DataFrame info
-        logger.info(f'{df}')
+# Create file handler
+fh = logging.FileHandler('sentiment_analysis.log')
+fh.setLevel(logging.INFO)
 
-    except Exception as e:
-        logger.error(f'Failed to read and log CSV file: {e}')
+# Create and set custom formatter
+formatter = CustomFormatter('%(ticker)s - Body Sentiment: %(body_sentiment)s - Trend Sentiment: %(trend_sentiment)s')
+fh.setFormatter(formatter)
 
+# Add filter and handler to logger
+logger.addFilter(CustomFilter())
+logger.addHandler(fh)
 
-# Run the scheduler
+def log_sentiments(csv_file):
+    with open(csv_file, mode='r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            logger.info('Processing sentiment data',
+                        extra={'ticker': row['Quote'], 'body_sentiment': row['Body Sentiment'], 'trend_sentiment': row['Trend Sentiment']})
+
 if __name__ == "__main__":
-   log_csv_info()
+    log_sentiments('news_data_with_sentiments.csv')
